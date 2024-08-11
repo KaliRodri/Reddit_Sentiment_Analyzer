3b5231064ef77499d8eeedf8a13e6721d61d93ae
Rodrigo Costa <88461161+KaliRodri@users.noreply.github.com>
2024-08-11 15:46:36 -0300

    Update Sentiment_analysis.py
    Adicionado interface gráfica simples e melhoria no calculo do sentimento do post

--- Sentiment_analysis.py
+++ Sentiment_analysis.py
@@ -1,8 +1,7 @@
 import praw
 import config.config as config
-import os
+import tkinter as tk
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-
 
 def connect_reddit():
     try:
@@ -17,15 +16,22 @@ def connect_reddit():
         exit()
 
 
-def analyze_emotion(text):
+def analyze_emotion(title, body):
     analyzer = SentimentIntensityAnalyzer()
-    sentiment_scores = analyzer.polarity_scores(text)
-    if sentiment_scores['compound'] > 0:
-        return 'Positivo'
-    elif sentiment_scores['compound']  < 0:
-        return 'Negativo'
-    else:
-        return 'Neutro'
+   
+    title_sentiment = analyzer.polarity_scores(title)
+    body_sentiment = analyzer.polarity_scores(body) if body else {'compound': 0}
+    
+    combined_sentiment = (title_sentiment['compound'] + body_sentiment['compound']) /2
+    
+    if combined_sentiment > 0:
+        sentiment = 'Positivo'
+    elif combined_sentiment < 0:
+        sentiment = 'Negativo'
+    else: 
+        sentiment = 'Neutro'
+    
+    return sentiment
     
 
 def get_posts(reddit, search_term, limit=5):
@@ -37,36 +43,49 @@ def get_posts(reddit, search_term, limit=5):
         print(f"Erro na pesquisa dos posts: {e}")
         return []
     
-    
-def print_analysis_results(posts):
-    for post in posts:
-        sentiment = analyze_emotion(post.selftext)
-        print(f"Título: {post.title}")
-        if post.selftext:
-            print(f"Corpo do Texto: {post.selftext}")
-        else:
-            print("Esse post não contém corpo do Texto")
-        print(f'Sentimento: {sentiment}')
-        print('---')
-    
-    
-def main():
-   
-   reddit = connect_reddit()
-   repeat = "s"
-   os.system('cls')
-   while repeat.lower() == "s":
-    
-    search_term = input("Digite o que gostaria que fosse pesquisado no Reddit e veja a análise emocional dos posts: ").strip() 
+  
+def analyze_post():
+    search_term = entry.get().strip()
     if not search_term:
-        print("A pesquisa não pode ser vazia")
-        continue
+        result_text.insert(tk.END, "A pesquisa não pode ser vazia\n")
+        return  
+    
+    reddit = connect_reddit()
     posts = get_posts(reddit, search_term)
-    print_analysis_results(posts)
     
-    print("Gostaria de pesquisar novamente? Digite 'S' para Sim ou qualquer outra tecla para sair.")
-    repeat = input()
-    os.system('cls')
+    result_text.delete(1.0, tk.END)
+    for post in posts:
+        sentiments = analyze_emotion(post.title, post.selftext)
+        result_text.insert(tk.END, "Título: ", "bold",  f"{post.title}\n")
+        if post.selftext:
+            result_text.insert(tk.END, "Corpo do Texto: ", "bold",  f"{post.selftext}\n")
+        else:
+            result_text.insert(tk.END, "Esse post não contém corpo do Texto\n")
+        result_text.insert(tk.END, "Sentimento: ", "bold", f" {sentiments}\n")
+        result_text.insert(tk.END, "---\n")
+            
+# Configuração da janela principal                  
+root = tk.Tk()
+root.title("Reddit Sentiment Analyzer")
+root.geometry("800x600")
+
+# Configuração do Label 
+label = tk.Label(root, text="Digite o que gostaria que fosse pesquisado no Reddit e veja a análise emocional dos posts: ", font=("Arial" , 10, "bold"))
+label.pack(pady=10)
+
+entry = tk.Entry(root, width=50)
+entry.pack(pady=10)
+
+search_button = tk.Button(root, text="Buscar", command=analyze_post)
+search_button.pack(pady=10)
+
+# Configuração do widget Text
+result_text = tk.Text(root, wrap=tk.WORD, height=20, width=70,)
+result_text.pack(pady=10)
+
+
+# Configuração das tags para formatação
+result_text.tag_configure("bold", font=("Helvetica", 9, "bold"))
+
+root.mainloop()
     
-if __name__ == '__main__':
-    main()
